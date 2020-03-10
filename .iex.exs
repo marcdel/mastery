3bdcf53574dfd1fee4bd4b5eb00f5a124ec44486
Marc Delagrammatikas <delagrammatikas@gmail.com>
2020-03-10 11:49:04 -0700

    Creates quiz_manager and adds example Math quiz

--- .iex.exs
+++ .iex.exs
@@ -1,30 +1,3 @@
 alias Mastery.Core.{Question, Quiz, Response, Template}
-
-# Demo walk through
-
-generator = %{left: [1, 2], right: [1, 2]}
-
-checker = fn (sub, answer) ->
-  sub[:left] + sub[:right] == String.to_integer(answer)
-end
-
-quiz =
-  Quiz.new(title: "Addition", mastery: 2)
-  |> Quiz.add_template(
-       name: :single_digit_addition,
-       category: :addition,
-       instructions: "Add the numbers",
-       raw: "<%= @left %> + <%= @right %>",
-       generators: generator,
-       checker: checker
-     )
-|> Quiz.select_question()
-
-email = "user@email.com"
-response = Response.new(quiz, email, "0")
-quiz = Quiz.answer_question(quiz, response)
-
-quiz = Quiz.select_question(quiz)
-IO.inspect(quiz.current_question.asked)
-response = Response.new(quiz, email, "2")
-quiz = Quiz.answer_question(quiz, response)
+alias Mastery.Boundary.{QuizManager}
+alias Mastery.Examples.{Math}
--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Run the app locally with `iex -S mix`
 Run tests with `mix test` or `mix test --cover`
 
 Generate new dependency graph with `./generate_dep_graph.sh`
+
+Simple math example:
+
+1) `iex -S mix`
+1) `GenServer.start_link(QuizManager, %{}, name: QuizManager)`
+1) `QuizManager.build_quiz(title: :simple_math)`
+1) `QuizManager.add_template(:simple_math, Math.template_fields)`
+1) `QuizManager.lookup_quiz_by_title(:simple_math)`
